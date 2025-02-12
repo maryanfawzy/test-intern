6222bae618e3ac515d02f96ecb1675e6d4e623df
Maryan Fawzy <163450742+maryanfawzy@users.noreply.github.com>
2025-02-12 13:41:03 -0600

    i

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
